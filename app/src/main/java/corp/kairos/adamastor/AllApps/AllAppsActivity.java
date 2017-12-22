3c4817bd74ee8703f11cfcfaad92e7610b46c71e
Diogo Couto <diogo2couto@gmail.com>
2017-12-22 21:05:19 +0000

    Add animation to the transition

--- app/src/main/java/corp/kairos/adamastor/AllApps/AllAppsActivity.java
+++ app/src/main/java/corp/kairos/adamastor/AllApps/AllAppsActivity.java
@@ -70,4 +70,30 @@ public class AllAppsActivity extends Activity {
         });
     }
 
+
+    @Override
+    public void finish() {
+        super.finish();
+        overridePendingTransitionExit();
+    }
+
+    @Override
+    public void startActivity(Intent intent) {
+        super.startActivity(intent);
+        overridePendingTransitionEnter();
+    }
+
+    protected void overridePendingTransitionEnter() {
+        overridePendingTransition(R.anim.slide_from_right, R.anim.slide_to_left);
+    }
+
+    protected void overridePendingTransitionExit() {
+        overridePendingTransition(R.anim.slide_from_left, R.anim.slide_to_right);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        overridePendingTransitionExit();
+    }
 }
--- app/src/main/java/corp/kairos/adamastor/HomeActivity.java
+++ app/src/main/java/corp/kairos/adamastor/HomeActivity.java
@@ -1,8 +1,11 @@
 package corp.kairos.adamastor;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Vibrator;
 import android.view.View;
 
 import corp.kairos.adamastor.AllApps.AllAppsActivity;
@@ -16,7 +19,41 @@ public class HomeActivity extends Activity {
         setContentView(R.layout.activity_home);
     }
 
+    @Override
+    public void finish() {
+        super.finish();
+        overridePendingTransitionExit();
+    }
+
+    @Override
+    public void startActivity(Intent intent) {
+        super.startActivity(intent);
+        overridePendingTransitionEnter();
+    }
+
+    /**
+     * Overrides the pending Activity transition by performing the "Enter" animation.
+     */
+    protected void overridePendingTransitionEnter() {
+        overridePendingTransition(R.anim.slide_from_right, R.anim.slide_to_left);
+    }
+
+    /**
+     * Overrides the pending Activity transition by performing the "Exit" animation.
+     */
+    protected void overridePendingTransitionExit() {
+        overridePendingTransition(R.anim.slide_from_left, R.anim.slide_to_right);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        overridePendingTransitionExit();
+    }
+
     public void showAllAppsMenu(View v){
+        Vibrator vibe = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+        vibe.vibrate(50);
         Intent i = new Intent(this, AllAppsActivity.class);
         startActivity(i);
     }
