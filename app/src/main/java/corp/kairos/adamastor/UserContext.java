0fb40409bfba91722282099cf79819042a060765
Daniel Tavares <dfv.tavares@gmail.com>
2018-01-03 16:08:14 +0000

    Add more parameters to UserContext class (#3)
    Add more parameters to UserContext class

--- app/src/main/java/corp/kairos/adamastor/UserContext.java
+++ app/src/main/java/corp/kairos/adamastor/UserContext.java
@@ -1,9 +1,15 @@
 package corp.kairos.adamastor;
 
+import android.location.Location;
+
 import java.util.ArrayList;
+import java.util.GregorianCalendar;
 import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
 
 import corp.kairos.adamastor.AllApps.AppDetail;
+import corp.kairos.adamastor.AllApps.AppDetailComparator;
 
 /**
  * Created by kiko on 23-12-2017.
@@ -15,12 +21,14 @@ import corp.kairos.adamastor.AllApps.AppDetail;
 public class UserContext {
     private String contextName; //The name of the context
     private List<AppDetail> contextApps; //The list of apps associated with this context
+    private Location location;
+    private GregorianCalendar init;
+    private GregorianCalendar end;
 
     public UserContext(String contextName, List<AppDetail> contextApps) {
         this.contextName = contextName;
         this.contextApps = contextApps;
     }
-
     public String getContextName() {
         return contextName;
     }
@@ -28,4 +36,34 @@ public class UserContext {
     public List<AppDetail> getContextApps() {
         return contextApps;
     }
+    public void addApp(AppDetail app){contextApps.add(app);}
+    public void removeApp(AppDetail app){
+        contextApps.remove(app);
+    }
+    public boolean appExists(AppDetail app){
+        return contextApps.contains(app);
+
+    }
+    public void setTimes(GregorianCalendar init, GregorianCalendar end){
+        this.init=init;
+        this.end=end;
+    }
+
+    public GregorianCalendar getEnd() {
+        return end;
+    }
+
+    public GregorianCalendar getInit() {
+        return init;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location){
+        this.location = location;
+    }
+
 }
+
