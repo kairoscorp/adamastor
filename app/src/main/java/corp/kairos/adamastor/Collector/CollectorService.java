4acd4eedb03f043ca5923008af84480a7906ed73
A65059 <A65059@alunos.uminho.pt>
2018-02-09 11:23:49 +0000

    fix to not informing CollectorService of manual change. fix to statistics aggregating everything on same context.

--- app/src/main/java/corp/kairos/adamastor/Collector/CollectorService.java
+++ app/src/main/java/corp/kairos/adamastor/Collector/CollectorService.java
@@ -77,7 +77,8 @@ public class CollectorService extends Service implements GoogleApiClient.Connect
     private boolean checkLocation = false;
     private int userActivityNow = 1;
     private static int predictedContext = 1;
-    private static int predictInterval = 6 * 10;
+    //private static int predictInterval = 6 * 10;
+    private static int predictInterval = 6 * 1;
     private static int predictIteration = 0;
     private static boolean predictionActive;
     private static int etlIteration = 0;
